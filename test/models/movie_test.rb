717eedda9243693e5e58c4d43980cc6293aa274e
Lindsay Terchin <lterchin@gmail.com>
2018-11-05 15:23:55 -0800

    added validation testing for movies

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,62 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+
+  describe 'validations' do
+
+    it 'is valid when all fields are present' do
+
+      @movie = Movie.new(title: 'title', inventory: 3)
+      result = @movie.valid?
+
+      expect(result).must_equal true
+
+    end
+
+
+    it 'is invalid without any inventory' do
+
+      @movie = Movie.new(title: 'title')
+      result = @movie.valid?
+
+      expect(result).must_equal false
+      expect(@movie.errors.messages).must_include :inventory
+
+    end
+
+
+    it 'is invalid without a title' do
+
+      @movie = Movie.new(inventory: 2)
+      result = @movie.valid?
+
+      expect(result).must_equal false
+      expect(@movie.errors.messages).must_include :title
+
+    end
+
+
+    it 'is invalid when missing both required fields' do
+
+      @movie = Movie.new()
+      result = @movie.valid?
+
+      expect(result).must_equal false
+
+    end
+
+
+    it 'is invalid if inventory is not an integer' do
+
+      @movie = Movie.new(title: 'title', inventory: 'abc')
+      result = @movie.valid?
+
+      expect(result).must_equal false
+      expect(@movie.errors.messages).must_include :inventory
+
+    end
+
   end
+
 end
