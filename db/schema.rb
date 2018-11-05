1a194d6ad6f015bea5aa40871e50d2a1bc87d07e
Lindsay Terchin <lterchin@gmail.com>
2018-11-05 14:24:32 -0800

    added new table for rentals and added associations between models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_220715) do
+ActiveRecord::Schema.define(version: 2018_11_05_221710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 2018_11_05_220715) do
     t.string "overview"
     t.date "release_date"
     t.integer "inventory"
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
