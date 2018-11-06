eed76bd06dad240990286d93e275c24441180b64
Carly Reiter <yepodacarly@gmail.com>
2018-11-06 12:10:40 -0800

    jsonify of customers is working.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,14 +3,14 @@ class CustomersController < ApplicationController
   def index
     customers = Customer.all
     # render json: jsonify(customers), status: :ok
-    render json: customers.as_json( only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count] )
+    render json: jsonify(customers)
   end
 
 
 
   private
 
-  def jsonify(customer)
+  def jsonify(customer_data)
     return customer_data.as_json( only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count])
   end
 
