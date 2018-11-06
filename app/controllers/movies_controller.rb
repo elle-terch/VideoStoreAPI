0f6d0505ef20387ae7b30b504c95614030193b72
Lindsay Terchin <lterchin@gmail.com>
2018-11-06 12:25:06 -0800

    handled errors, added show method and added jsonify to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
   def index
     movies = Movie.all
 
-    render json: movies.as_json( only: [:id, :title, :release_date])
+    render json: jsonify(movies)
 
   end
 
@@ -14,13 +14,26 @@ class MoviesController < ApplicationController
     else
       render_error(:bad_request, movie.errors.messages )
     end
-
   end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+    if movie
+      render json: jsonify(movie)
+    # else
+      # render_error(:not_found, { mov})
+    end
+  end
+
 
   private
 
   def movie_params
     params.require(:movie).permit(:title, :overview, :release_date, :inventory)
+  end
+
+  def jsonify(movie_data)
+    return movie_data.as_json( only: [:id, :title, :release_date] )
   end
 
 end
