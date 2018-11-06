44a22cf5c154dc9b32dc1ee633332574beaf6d31
Lindsay Terchin <lterchin@gmail.com>
2018-11-06 15:47:19 -0800

    created index and show action for rentals

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,6 +3,15 @@ class RentalsController < ApplicationController
   def index
 
     rentals = Rental.all
+    render json: rentals.as_json( except: [:created_at, :updated_at] )
 
+  end
+
+  def show
+    rental_id = params[:id]
+    rental = Rental.find_by(id: rental_id)
+    if rental
+      render json: rental.as_json( except: [:created_at, :updated_at] )
+    end
   end
 end
