3f0e007c4010d1498988af746e7217ab2ba96952
Lindsay Terchin <lterchin@gmail.com>
2018-11-06 11:47:26 -0800

    updated show route for movie

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get '/movies', to: 'movies#index', as: 'movies'
 
-  get 'movies', to: 'movies#show', as: 'movie'
+  get '/movies/:id', to: 'movies#show', as: 'movie'
 
   post '/movies', to: 'movies#create'
 
