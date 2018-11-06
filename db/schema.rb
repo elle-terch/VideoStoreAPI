ffb5f3f4cd728cf458910bd96e816ff6d5bf2280
Lindsay Terchin <lterchin@gmail.com>
2018-11-05 16:49:36 -0800

    added movies_checked_out_column to movies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_225120) do
+ActiveRecord::Schema.define(version: 2018_11_06_004821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_11_05_225120) do
     t.string "overview"
     t.date "release_date"
     t.integer "inventory"
+    t.integer "movies_checked_out_count", default: 0
   end
 
   create_table "rentals", force: :cascade do |t|
