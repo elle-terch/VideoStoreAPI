b7589103f7b2ee408d513247965d168e2b610ca7
Lindsay Terchin <lterchin@gmail.com>
2018-11-05 16:17:31 -0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
 
 
-get '/zomg', to: 'zomg#index'
+  get '/zomg', to: 'zomg#index'
+
+  get '/customers', to: 'customers#index', as: 'customers'
+
+  get '/movies', to: 'movies#index', as: 'movies'
+
+  get 'movies', to: 'movies#show', as: 'movie'
+
+  post '/movies', to: 'movies#create'
+
+
 
 
 end
