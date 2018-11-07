ac4d9519984b4927d6ab580cfdd8869b2c2e0fea
Lindsay Terchin <lterchin@gmail.com>
2018-11-06 16:17:46 -0800

    added checkout method and strong params

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -7,6 +7,7 @@ class RentalsController < ApplicationController
 
   end
 
+
   def show
     rental_id = params[:id]
     rental = Rental.find_by(id: rental_id)
@@ -14,4 +15,25 @@ class RentalsController < ApplicationController
       render json: rental.as_json( except: [:created_at, :updated_at] )
     end
   end
+
+
+  def checkout
+    rental = Rental.new(rental_params)
+    if rental.save
+      render json: { id: rental.id }
+      #should we be validating movie & customer ids?
+    end
+  end
+
+
+  def checkin
+
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:movie_id, :customer_id )
+  end
+
 end
